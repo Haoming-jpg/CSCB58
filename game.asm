c5bd5dc2adafc77dbca60ba0c6ea063c6b8fa21a
Haoming-jpg <85337319+Haoming-jpg@users.noreply.github.com>
2022-04-06 13:52:03 -0400

    Add files via upload
    Moving platform 0.1

--- game.asm
+++ game.asm
@@ -14,11 +14,11 @@
 #
 # Which milestones have been reached in this submission?
 # (See the assignment handout for descriptions of the milestones)
-# - Milestone 1/2/3 (choose the one the applies)
+# - Milestone 3 
 #
 # Which approved features have been implemented for milestone 3?
 # (See the assignment handout for the list of additional features)
-# 1. (fill in the feature, if any)
+# 1. Fail condition
 # 2. (fill in the feature, if any)
 # 3. (fill in the feature, if any)
 # ... (add more if necessary)
@@ -48,13 +48,19 @@
 .eqv CAT_X_LEN 9
 .eqv CAT_Y_LEN 7
 .eqv SLEEP 40
-.eqv GRAVITY_LOOP 5
+.eqv GRAVITY_LOOP 8
 .eqv points 0x1000DC00		# x = 60
 .eqv FIRST_G 0x10009944 	# 25, 17
 .eqv SECOND_G 0x10009994 	# 25, 37
 .eqv L_COIN 252		# left offset of coin
 .eqv R_COIN 260		# right offset of coin
 .eqv D_COIN 512		# down offset of coin
+.eqv PF1 0x10008B1C		# x = 11 y = 7
+.eqv PF2 0x10009864 		# x = 24, y = 25 offset 6244
+.eqv PF3 0x1000A530		# x = 37 y = 12 offset 9520
+.eqv PF4 0x1000B2B0		# x = 50, y = 44 offset 12976
+
+
 
 
 .text
@@ -125,7 +131,7 @@ main:
 	sw $t3, 2352($t0)
 	sw $t1, 2356($t0)
 	
-
+	
 	sw $t1, 2596($t0)
 	sw $t4, 2600($t0)
 	sw $t1, 2604($t0)
@@ -133,21 +139,20 @@ main:
 	sw $t1, 2612($t0)
 	sw $t4, 2616($t0)
 	
-	# draw 3 basic platform
-	# first one x = 11 y = 7
-	li $a0, 0x10008B1C
+	# draw 4 basic platform
+	li $a0, PF1
 	jal draw_platform
 
 	#second one, x = 24, y = 25 offset 6244
-	li $a0, 0x10009864
+	li $a0, PF2
 	jal draw_platform
 	
 	#third one, x = 37 y = 12 offset 9520
-	li $a0, 0x1000A530
+	li $a0, PF3
 	jal draw_platform
 	
 	# forth one, x = 50, y = 44 offset 12976
-	li $a0, 0x1000B2B0
+	li $a0, PF4
 	jal draw_platform
 	
 	#draw fire at bottom
@@ -216,8 +221,10 @@ draw_right_wall:
 
 
 	li $s0, CAT_INITIAL	# t7 = address of initial cat(top left)
-	li $s6, GRAVITY_LOOP		# set the loop of gravity
+	li $s6, GRAVITY_LOOP	# set the loop of gravity
 	li $s5, 0		# if s5 = -1 then gg
+	li $s7, 0		# s7 is for moving platformn, if 0 then go right if 1 then go left
+	li $s4, PF3		# PF3 is the mvoing platform
 	
 main_loop:
 	# main loop of game
@@ -234,11 +241,10 @@ not_gg:
 	
 	
 	beqz $s6, do_gravity
+finish_gravity:
+	beqz $s6, move_platform
 	addi $s6, $s6, -1
-	
-continue_main_loop:
-
-
+finish_move_platform:
 
 
 sleep:
@@ -248,13 +254,12 @@ sleep:
         
         j main_loop
 do_gravity:
-	li $s6, GRAVITY_LOOP		# set the loop of gravity
 	addi $sp, $sp, -4	# push the address of cat to the stack
 	sw $s0, 0($sp)
 	jal gravity
 	lw $s0, 0($sp)		# get the new address of cat
 	addi $sp, $sp, 4
-	j continue_main_loop
+	j finish_gravity
 
 check_gg:
 	add $t0, $s0, $zero	# t0 stores the address of the cat
@@ -279,6 +284,20 @@ gg:
 	jal clean_all
 	jal draw_gg
 	j sleep
+
+
+move_platform:
+	#s4 is the address of the platform
+	li $s6, GRAVITY_LOOP
+	
+	
+	
+	add $a0, $s4, $zero
+	jal remove_platform
+	addi $s4, $s4, 4
+	add $a0, $s4, $zero
+	jal draw_platform
+	j finish_move_platform
 
 #####################################################################
 # int keypress_happened(int cat_address) returns the new cat address
@@ -788,6 +807,23 @@ darw_platform_loop:
 	
 	jr $ra
 #####################################################################
+
+#####################################################################
+remove_platform:
+	add $t7, $zero, $a0
+	li $t6, BLACK
+	li $t8, 40
+	
+	
+remove_platform_loop:
+	sw $t6, 0($t7)
+	addi $t7, $t7, 4
+	addi $t8, $t8, -4
+	bnez $t8, remove_platform_loop
+	
+	jr $ra
+#####################################################################
+
 #####################################################################
 END_PROGRAM:
 	li $v0, 10 # terminate the program gracefully
